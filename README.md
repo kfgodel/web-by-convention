1b72c4c583ffcdadaa36853d1f94cf63e2938b9f
Dario L Garcia <dario.garcia@kfgodeldev.com>
2015-02-19 01:32:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# web-by-convention
+
+Jetty based executable server using convention over configuration to write web apps really easily
+
+
---
+++ src/main/java/ar/com/kfgodel/webbyconvention/WebServer.java
@@ -0,0 +1,50 @@
+package ar.com.kfgodel.webbyconvention;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.resource.Resource;
+import org.glassfish.jersey.jetty.JettyHttpContainer;
+import org.glassfish.jersey.server.ContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.Optional;
+
+/**
+ * This type represents the simplified by pre-definitions jetty server 
+ * Created by kfgodel on 19/02/15.
+ */
+public class WebServer {
+    
+    private Server jettyServer;
+    
+    private void initialize(Optional<Integer> predefinedPort) {
+        this.jettyServer = new Server(predefinedPort.orElse(80));
+
+        // Serve as static content everything under classpath:/web/
+        ResourceHandler webHandler = new ResourceHandler();
+        webHandler.setBaseResource(Resource.newClassPathResource("/web"));
+
+        // Use as resource definitions everything inside that package
+        ResourceConfig config = new ResourceConfig()
+                .packages(true, "ar.com.kfgodel.web.resources");
+        final JettyHttpContainer jerseyHandler = ContainerFactory.createContainer(JettyHttpContainer.class, config);
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { webHandler, jerseyHandler });
+        jettyServer.setHandler(handlers);
+    }
+
+    public void startAndJoin() throws Exception {
+        jettyServer.start();
+        jettyServer.join();
+    }
+
+    public static WebServer create(Optional<Integer> predefinedPort) {
+        WebServer webServer = new WebServer();
+        webServer.initialize(predefinedPort);
+        return webServer;
+    }
+
+}
